1893f078c3a03989f7a294425b128dcc8161981b
onthedesk <xiuguozhao@gmail.com>
2013-09-03 11:38:18 +0800

    init repo of happermakeup

---
+++ include/configure/db.php
@@ -0,0 +1,8 @@
+<?php
+$value = array (
+  'host' => '110.75.188.60',
+  'user' => 'xiaofeng',
+  'pass' => 'xiaofengoutside',
+  'name' => 'happermakeup',
+);
+?>
---
+++ include/configure/system.php
@@ -0,0 +1,27 @@
+<?php
+$value = array (
+  'wwwprefix' => 'http://www.xuelu.org/hm',
+  'imgprefix' => 'http://www.xuelu.org/hm',
+  'cssprefix' => 'http://www.xuelu.org/hm',
+  'sitename' => 'Happer Makeup',
+  'sitetitle' => '',
+  'abbreviation' => 'Happer Makeup',
+  'couponname' => '优惠券',
+  'currency' => '¥',
+  'currencyname' => 'CNY',
+  'timezone' => 'Etc/GMT-8',
+  'description' => '',
+  'keywords' => '',
+  'invitecredit' => '0',
+  'conduser' => 0,
+  'partnerdown' => 0,
+  'gzip' => 0,
+  'kefuqq' => '',
+  'kefumsn' => '',
+  'mobileurl' => '',
+  'icp' => '',
+  'statcode' => '',
+  'sinajiwai' => '',
+  'tencentjiwai' => '',
+);
+?>
